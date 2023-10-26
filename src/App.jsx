c485115f3c22913ec3f41aee94032791617bae31
Taylor jr <luisdavidferrerconde@gmail.com>
2023-10-25 21:54:07 -0500

    fix location app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import { DrummerHouse } from './components/Drummerhouse'
+import { DrummerHouse } from '../src/components/DrummerHouse/index'
 
 function App() {
 
