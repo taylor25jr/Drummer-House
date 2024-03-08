89d7eb3c15a1498a5560dd4fbd70c882ebe174d7
Taylor25jr <luisdavidferrerconde@gmail.com>
2024-03-08 14:44:48 -0500

    feat, burgermenu aria-label defined

--- src/components/MobileHeader/index.jsx
+++ src/components/MobileHeader/index.jsx
@@ -25,6 +25,7 @@ export default function MobileNavbar({
         variant="outlined"
         color="neutral"
         onClick={() => setOpen(true)}
+        aria-label="burgerButton"
       >
         <Menu />
       </IconButton>
