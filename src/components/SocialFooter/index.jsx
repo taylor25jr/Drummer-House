da81577ade9f2740c284d880d80e3e5d36beeec8
Taylor25jr <luisdavidferrerconde@gmail.com>
2024-03-08 14:42:02 -0500

    feat, icons aria-label defined

--- src/components/SocialFooter/index.jsx
+++ src/components/SocialFooter/index.jsx
@@ -8,7 +8,7 @@ import { Link } from "react-router-dom";
 export const SocialFooter = () => {
 
   const routes = {
-    facebook: "https://www.facebook.com/search/top?q=johan%20sebastian",
+    facebook: "https://www.facebook.com/profile.php?id=100064800582592&locale=es_LA",
     instagram: "https://www.instagram.com/johan_sebas00/",
     whatsapp: "https://api.whatsapp.com/send?phone=3008559576&text=hola,soy luis"
   }
@@ -17,13 +17,13 @@ export const SocialFooter = () => {
 
   return (
     <div className="social__footer">
-      <Link to={facebook} target="_blank">
+      <Link to={facebook} target="_blank" aria-label="go to facebook">
         <FacebookIcon />
       </Link>
-      <Link to={instagram} target="_blank">
+      <Link to={instagram} target="_blank" aria-label="go to instagram">
         <InstagramIcon />
       </Link>
-      <Link to={whatsapp} target="_blank">
+      <Link to={whatsapp} target="_blank" aria-label="go to whatsapp">
         <WhatsappIcon/>
       </Link>
     </div>
